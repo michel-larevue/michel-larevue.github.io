d906c5339fa9716239fc066b5d16d152ca9b1907
ApolloNet <alex@apollonet.fr>
2015-10-22 17:14:47 +0200

    feat: brunch pour le build frontend

---
+++ brunch-config.js
@@ -0,0 +1,12 @@
+exports.config = {
+  'optimize': true,
+  'paths': {
+    'watched': ['less'],
+    'public': ''
+  },
+  'files': {
+    'stylesheets': {
+      'joinTo': 'css/main.css'
+    }
+  }
+};
